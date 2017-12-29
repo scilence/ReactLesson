3ce2a1113b8d0620c8a2f04cc405a53845b4ba19
07243.ben.wang <scilence.wang@gmail.com>
2017-12-29 17:00:37 +0800

    add router

--- Sample01/src/js/app.js
+++ Sample01/src/js/app.js
@@ -1,20 +1,39 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
+import {BrowserRouter, Route, Link} from 'react-router-dom';
+import createBrowserHistory from 'history/createBrowserHistory'
+
+import About from './About';
 
 import '../css/style.css';
 
 import reactIcon from '../assets/react-icon.png';
 
-export default class Hello extends Component {
-  render() {
+
+const history = createBrowserHistory()
+
+export default class App extends Component{
+  render(){
     return (
       <div>
-        Hello from react
-
+        <h1>Hello World</h1>
+        <nav>
+          <Link to="/about" >About</Link>
+        </nav>
         <img src={ reactIcon } alt='React Icon' />
       </div>
     );
   }
 }
 
-render(<Hello />, document.getElementById('app'));
+render((
+  <div>
+    <BrowserRouter basename="/">
+      <div>
+        <Route exact path="/" component={App}/>
+        <Route path="/about" component={About}/>
+      </div>
+    </BrowserRouter>
+  </div>
+  )
+  , document.getElementById('app'));
