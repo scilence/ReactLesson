d562117371908f37199b46f81dede9011bcc8f1f
Ben Wang <scilence.wang@gmail.com>
2018-01-04 00:44:07 +0800

    modify tabs

--- Sample02/src/js/app.js
+++ Sample02/src/js/app.js
@@ -8,17 +8,31 @@ import Index from './index';
 import '../css/style.css';
 
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import injectTapEventPlugin from "react-tap-event-plugin";
 // import MyAwesomeReactComponent from './MyAwesomeReactComponent';
+import Tabs, { Tab } from "material-ui/tabs";
+
+injectTapEventPlugin();
 
 const supportHistroy = "pushState" in window.history;
 
 const User = ({match}) => { return <h1>Hello {match.params.username}</h1> };
+
+const handleTabChanged = (value) => {
+  console.log(value);
+}
 
 render((
   <div>
     <MuiThemeProvider>
       <BrowserRouter basename="/" forceRefresh={!supportHistroy}>
         <div>
+
+          <Tabs onChange={handleTabChanged}>
+            <Tab label="Home" value="home"/>
+            <Tab label="About" value="about" />
+          </Tabs>
+
           <Route path="/" component={Index}/>
           <Switch>
               <Route path="/about" component={About}/>
--- Sample02/src/js/index.js
+++ Sample02/src/js/index.js
@@ -1,10 +1,6 @@
 import React , { Component } from "react"; 
-import injectTapEventPlugin from "react-tap-event-plugin";
 //import Taps from "material-ui/lib/tabs/tabs";
 // import Tap from "material-ui/lib/tabs/tab";
-import Tabs, { Tab } from "material-ui/tabs";
-
-injectTapEventPlugin();
 
 
 export default class Index extends Component{
@@ -12,10 +8,6 @@ export default class Index extends Component{
         return (
             <div>
                 <h1>Index</h1>
-                <Tabs>
-                    <Tab label="Home" />
-                    <Tab label="About" />
-                </Tabs>
             </div>
         );
     }
